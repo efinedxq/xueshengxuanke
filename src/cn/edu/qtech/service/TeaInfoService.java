db8e3a7317b81935047cca8e5c4322347b1a6524
efinedxq <efinedxq@DESKTOP-MD145LD>
2017-06-30 11:09:20 +0800

    增加 密码加密

--- src/cn/edu/qtech/service/TeaInfoService.java
+++ src/cn/edu/qtech/service/TeaInfoService.java
@@ -9,54 +9,31 @@ import cn.edu.qtech.util.Converter;
 import cn.edu.qtech.util.Md5Utils;
 
 public class TeaInfoService {
-    private TeaInfoDao teaInfoDao;
+	private TeaInfoDao teaInfoDao;
+
 	public TeaInfoService() {
 		teaInfoDao = new TeaInfoDao();
 	}
+
 	/**
 	 * 添加成功则返回 1,否则返回 0;
+	 * 
 	 * @param map
 	 * @return boolean
 	 */
-	public boolean doSave(Map<String, String[]> map){
-		boolean flag=false;
-		Map<String,String> param = Converter.convertMap(map);
-		System.out.println(param);
-		try {
-			int row=teaInfoDao.insert(param);
-			if(row>0){
-				flag=true;
-			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return flag;
-	}
-	/**
-	 * 选取全部的管理员
-	 * @return list<map<String,String>> list
-	 */
-	public List<Map<String,String>> findAll(){
-		List<Map<String,String>> list=
-				teaInfoDao.select(null);
-		return list;
-	}
-	/**
-	 * 删除教师   删除成功是 true
-	 * @param map
-	 * @return
-	 */
-	public boolean doDelete(Map<String,String[]> map){
+	public boolean doSave(Map<String, String[]> map) {
 		boolean flag = false;
-		//构建sql语句的条件部分
-		Map<String,String> where = Converter.convertMap(map);
-		//构建修改的列 和其对应的值，把状态列的值 由 1 修改为 0
-		Map<String,String> param = new HashMap<String,String>();
-		param.put("t_status", "0");
+		Map<String, String> param = Converter.convertMap(map);
+		System.out.println(param);
+		// 获取原始的密码数据
+		String pass = param.get("t_pass");
+		// 密码加密
+		String new_pass = Md5Utils.md5(pass);
+		// 替换原来的密码
+		param.put("t_pass", new_pass);
 		try {
-			int row = teaInfoDao.update(param, where);
-			if(row>0) {
+			int row = teaInfoDao.insert(param);
+			if (row > 0) {
 				flag = true;
 			}
 		} catch (Exception e) {
@@ -65,8 +42,45 @@ public class TeaInfoService {
 		}
 		return flag;
 	}
+
+	/**
+	 * 选取全部的管理员
+	 * 
+	 * @return list<map<String,String>> list
+	 */
+	public List<Map<String, String>> findAll() {
+		List<Map<String, String>> list = teaInfoDao.select(null);
+		return list;
+	}
+
+	/**
+	 * 删除教师 删除成功是 true
+	 * 
+	 * @param map
+	 * @return
+	 */
+	public boolean doDelete(Map<String, String[]> map) {
+		boolean flag = false;
+		// 构建sql语句的条件部分
+		Map<String, String> where = Converter.convertMap(map);
+		// 构建修改的列 和其对应的值，把状态列的值 由 1 修改为 0
+		Map<String, String> param = new HashMap<String, String>();
+		param.put("t_status", "0");
+		try {
+			int row = teaInfoDao.update(param, where);
+			if (row > 0) {
+				flag = true;
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return flag;
+	}
+
 	/**
 	 * 修改教师信息后更新
+	 * 
 	 * @param map
 	 * @return
 	 */
@@ -86,28 +100,28 @@ public class TeaInfoService {
 		}
 		return flag;
 	}
+
 	/**
-	 * 教师登录
-	 * 根据用户名 密码登录。
+	 * 教师登录 根据用户名 密码登录。
+	 * 
 	 * @param map
 	 * @return
 	 */
-	public Map<String,String> login(Map<String, String[]> map){
-		Map<String,String> login = new HashMap<String,String>();
-		
-		Map<String,String> param = Converter.convertMap(map);
-		//获取原始的密码数据
+	public Map<String, String> login(Map<String, String[]> map) {
+		Map<String, String> login = new HashMap<String, String>();
+
+		Map<String, String> param = Converter.convertMap(map);
+		// 获取原始的密码数据
 		String pass = param.get("t_pass");
-		//密码加密
+		// 密码加密
 		String new_pass = Md5Utils.md5(pass);
-		//替换原来的密码
+		// 替换原来的密码
 		param.put("t_pass", new_pass);
 		System.out.println(param);
 		try {
-			List<Map<String,String>> list=
-					teaInfoDao.select(param);
+			List<Map<String, String>> list = teaInfoDao.select(param);
 			System.out.println(list);
-			if(list!=null||list.size()>0){
+			if (list != null || list.size() > 0) {
 				login = list.get(0);
 			}
 		} catch (Exception e) {
@@ -116,17 +130,19 @@ public class TeaInfoService {
 		}
 		return login;
 	}
+
 	/**
 	 * 获取一个 教师 信息
+	 * 
 	 * @param map
 	 * @return
 	 */
-	public Map<String,String> oneTea(Map<String, String[]> map){
-		Map<String,String> stu = new HashMap<String,String>();
-		Map<String,String> param = Converter.convertMap(map);
+	public Map<String, String> oneTea(Map<String, String[]> map) {
+		Map<String, String> stu = new HashMap<String, String>();
+		Map<String, String> param = Converter.convertMap(map);
 		try {
-			List<Map<String,String>> list= teaInfoDao.select(param);
-			if(list!=null||list.size()>0){
+			List<Map<String, String>> list = teaInfoDao.select(param);
+			if (list != null || list.size() > 0) {
 				stu = list.get(0);
 			}
 		} catch (Exception e) {
@@ -135,5 +151,5 @@ public class TeaInfoService {
 		}
 		return stu;
 	}
-	
+
 }
